3c7d9eacbee37d839364ec65c598f375fdef5cef
Yusif Alizada <yusifedu@gmail.com>
2020-04-19 16:16:23 +0400

    shopping app created

---
+++ projects/year2/spring/shopping/src/test/java/az/developia/shopping/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
