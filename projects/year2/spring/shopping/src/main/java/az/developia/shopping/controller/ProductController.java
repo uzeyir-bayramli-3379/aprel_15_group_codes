85b7d9c85bcadf0bc46760b0166df580ac183afb
Yusif Alizada <yusifedu@gmail.com>
2020-04-22 20:50:04 +0400

    Update ProductController.java
    get api test

--- projects/year2/spring/shopping/src/main/java/az/developia/shopping/controller/ProductController.java
+++ projects/year2/spring/shopping/src/main/java/az/developia/shopping/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,4 +27,17 @@ public class ProductController {
 		product.setId(null);
 		productDAO.save(product);
 	}
+	
+	@GetMapping
+	public List<Product> findAll(){
+return productDAO.findAll();
+	}
+	
+	
+	@GetMapping(path="/{id}")
+	public  Product  findById(@PathVariable(name="id") Integer id){
+return productDAO.findById(id).get();
+	}
+	
+	
 }
