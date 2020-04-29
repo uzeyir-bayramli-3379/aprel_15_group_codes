621e726ed0b1416a70a8dfe1e0efab18d77b6bd6
Yusif Alizada <yusifedu@gmail.com>
2020-04-29 19:36:48 +0400

    delete and put api

--- projects/year2/spring/shopping/src/main/java/az/developia/shopping/controller/ProductController.java
+++ projects/year2/spring/shopping/src/main/java/az/developia/shopping/controller/ProductController.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +41,13 @@ return productDAO.findAll();
 return productDAO.findById(id).get();
 	}
 	
-	
+	@DeleteMapping(path="/{id}")
+	public void delete(@PathVariable(name="id") Integer id){
+		productDAO.deleteById(id);
+	}
+	@PutMapping
+	public void update(@RequestBody Product product){
+	 
+		productDAO.save(product);
+	}
 }
