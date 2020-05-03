5a17539dd61a45478ba8d2ef13a6093c8323e1e1
Yusif Alizada <yusifedu@gmail.com>
2020-05-03 17:28:36 +0400

    Update ProductController.java

--- projects/year2/spring/shopping/src/main/java/az/developia/shopping/controller/ProductController.java
+++ projects/year2/spring/shopping/src/main/java/az/developia/shopping/controller/ProductController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import az.developia.shopping.model.Product;
 
 @RestController
 @RequestMapping(path="/products")
+@CrossOrigin(origins = "*")
 public class ProductController {
  
 
