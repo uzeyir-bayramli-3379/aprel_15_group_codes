c28f8c868691b829fb08b43dbb0e046c89f142cb
yalizada <37800222+yalizada@users.noreply.github.com>
2020-05-31 15:45:09 +0200

    menu step 1

--- projects/year2/frontend/ts/my-project/src/app/app.module.ts
+++ projects/year2/frontend/ts/my-project/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { SalesPersonListComponent } from './sales-person-list/sales-person-list.component';
+import { MenuComponent } from './component/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SalesPersonListComponent
+    SalesPersonListComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
