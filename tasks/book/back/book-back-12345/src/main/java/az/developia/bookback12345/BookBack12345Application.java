4f1977f01f14f3e7f5c35db537428e831f7a8edf
Yusif Alizada <yusifedu@gmail.com>
2020-07-08 19:56:03 +0400

    book project created

---
+++ tasks/book/back/book-back-12345/src/main/java/az/developia/bookback12345/BookBack12345Application.java
@@ -0,0 +1,13 @@
+package az.developia.bookback12345;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBack12345Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBack12345Application.class, args);
+	}
+
+}
---
+++ tasks/book/back/book-back-12345/src/test/java/az/developia/bookback12345/BookBack12345ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.bookback12345;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBack12345ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
