312310352a186ed60fa3ac58e9755b6c14d9342f
Yusif Alizada <yusifedu@gmail.com>
2020-09-09 20:31:40 +0400

    Update ProductRESTController.java
    mehsul ucun post veb servisi yazdiq

--- online-shopping/back/shopping-muellim/src/main/java/az/developia/shoppingmuellim/controller/ProductRESTController.java
+++ online-shopping/back/shopping-muellim/src/main/java/az/developia/shoppingmuellim/controller/ProductRESTController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,9 @@ public class ProductRESTController {
 public List<Product> findAll(){
 	return productDAO.findAll();
 }
-	
+	@PostMapping
+	public Integer add(@RequestBody Product product) {
+		product.setId(null);
+		return productDAO.save(product).getId();
+	}
 }
