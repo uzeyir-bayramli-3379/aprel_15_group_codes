ac0c74de3ba72fc25c2c24eca230fd7205057e62
Yusif Alizada <yusifedu@gmail.com>
2020-09-09 19:42:16 +0400

    back project create
    online shopping hayihesinin back proyekt fayllari yaradildi

---
+++ online-shopping/back/shopping-muellim/src/main/java/az/developia/shoppingmuellim/ShoppingMuellimApplication.java
@@ -0,0 +1,13 @@
+package az.developia.shoppingmuellim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMuellimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMuellimApplication.class, args);
+	}
+
+}
---
+++ online-shopping/back/shopping-muellim/src/test/java/az/developia/shoppingmuellim/ShoppingMuellimApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.shoppingmuellim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMuellimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
