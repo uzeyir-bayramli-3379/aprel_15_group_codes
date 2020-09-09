3e6d3c0363c0ff5fe799125ee443e3737531570e
Yusif Alizada <yusifedu@gmail.com>
2020-09-09 20:34:50 +0400

    Update ProductRESTController.java
    product find by id

--- online-shopping/back/shopping-muellim/src/main/java/az/developia/shoppingmuellim/controller/ProductRESTController.java
+++ online-shopping/back/shopping-muellim/src/main/java/az/developia/shoppingmuellim/controller/ProductRESTController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,4 +30,16 @@ public List<Product> findAll(){
 		product.setId(null);
 		return productDAO.save(product).getId();
 	}
+	
+	
+	@PutMapping
+	public void update(@RequestBody Product product) {
+		 
+		  productDAO.save(product);
+	}
+	@GetMapping(path="/{id}") // http://localhost:8204/products/4 - GET
+	public  Product  findById(@PathVariable(name="id") Integer id){
+		return productDAO.findById(id).get();
+	}
+	
 }
